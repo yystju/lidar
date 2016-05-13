c892cfa85b148f7fe02ab6ebf7ef5f9e278af91c
SHI, QUAN <yystju@hotmail.com>
2016-05-13 16:22:06 +0000

    Update several test.

--- src/serial.c
+++ src/serial.c
@@ -1,103 +1,54 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include <errno.h>
-#include <termios.h>
-#include <unistd.h>
-#include <fcntl.h>
-
-int
-set_interface_attribs (int fd, int speed, int parity)
+int main()
 {
-        struct termios tty;
-        memset (&tty, 0, sizeof tty);
-        if (tcgetattr (fd, &tty) != 0)
+    int fd,readnum;
+    char *dev="/dev/ttyAMA0",recv[1024*4];
+    struct termios opt,oldopt;
+
+    fd = open( dev, O_RDWR ); 
+
+    tcgetattr( fd,&oldopt);
+
+    tcgetattr( fd,&opt);
+    cfsetispeed(&opt, B4800);//9600
+    cfsetospeed(&opt, B4800);
+
+    opt.c_cflag &= ~PARENB; //N
+    opt.c_cflag &= ~INPCK;
+    opt.c_cflag &= ~CSTOPB;//1
+    opt.c_cflag &= ~CSIZE;
+    opt.c_cflag |= CS8; //8
+
+    opt.c_iflag &= ~(IXON | IXOFF | IXANY);
+
+    opt.c_cc[VTIME] = 0; 
+    opt.c_cc[VMIN] = 0; 
+
+    opt.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); /*Input*/
+    opt.c_oflag &= ~OPOST; /*Output*/
+
+    tcflush(fd, TCIOFLUSH);
+    tcsetattr(fd, TCSANOW, &opt);
+
+    while(1)
+    {
+        bzero(recv,sizeof(recv));
+        while((readnum = read(fd,recv,sizeof(recv)))>0)
         {
-                fprintf(stderr, "error %d from tcgetattr", errno);
-                return -1;
+            printf("%s",recv);
         }
+    }
 
-        cfsetospeed (&tty, speed);
-        cfsetispeed (&tty, speed);
-
-        tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
-        // disable IGNBRK for mismatched speed tests; otherwise receive break
-        // as \000 chars
-        tty.c_iflag &= ~IGNBRK;         // disable break processing
-        tty.c_lflag = 0;                // no signaling chars, no echo,
-                                        // no canonical processing
-        tty.c_oflag = 0;                // no remapping, no delays
-        tty.c_cc[VMIN]  = 0;            // read doesn't block
-        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
-
-        tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
-
-        tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
-                                        // enable reading
-        tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
-        tty.c_cflag |= parity;
-        tty.c_cflag &= ~CSTOPB;
-        tty.c_cflag &= ~CRTSCTS;
-
-        if (tcsetattr (fd, TCSANOW, &tty) != 0)
-        {
-                fprintf(stderr, "error %d from tcsetattr", errno);
-                return -1;
-        }
-        return 0;
-}
-
-void
-set_blocking (int fd, int should_block)
-{
-        struct termios tty;
-        memset (&tty, 0, sizeof tty);
-        if (tcgetattr (fd, &tty) != 0)
-        {
-                fprintf(stderr, "error %d from tggetattr", errno);
-                return;
-        }
-
-        tty.c_cc[VMIN]  = should_block ? 1 : 0;
-        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
-
-        if (tcsetattr (fd, TCSANOW, &tty) != 0)
-            fprintf(stderr, "error %d setting term attributes", errno);
-}
-
-int
-main(int argc, char * argv[]) {
-	char *portname = "/dev/ttyAMA0";
-	
-	fprintf(stdout, "portname : %s\n", portname);
-
-	int fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
-	
-	fprintf(stdout, "FD : 0x%x\n", fd);
-	
-	if (fd < 0)
-	{
-			fprintf(stderr, "error %d opening %s: %s", errno, portname, strerror (errno));
-			return;
-	}
-	
-	set_interface_attribs (fd, B9600, 0);  // set speed to 115,200 bps, 8n1 (no parity)
-	set_blocking (fd, 0);                // set no blocking
-
-	//write (fd, "hello!\n", 7);           // send 7 character greeting
-
-	usleep ((7 + 25) * 100);             // sleep enough to transmit the 7 plus
-								 // receive 25:  approx 100 uS per char transmit
-	char buf[1024*8];
-	
-	
-	while(1) {
-		memset(buf, 0, sizeof(buf));
-		
-		int n = read (fd, buf, sizeof(buf));  // read up to 100 characters if ready to read
-		
-		fprintf(stdout, "%s", buf);
-	}
-	return 0;
+    tcflush(fd, TCIOFLUSH);
+    tcsetattr(fd, TCSANOW, &oldopt); 
+    close(fd);
 }
