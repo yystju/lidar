1abad808098e22893c5b7690c904ddc5dd7006d1
v0cn305 <v0cn305@volvo.com>
2016-07-08 11:21:34 +0800

    Update Lidar code.

--- new/lidar.c
+++ new/lidar.c
@@ -1,44 +1,104 @@
 #include "lidar.h"
+#include "helper.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include <stdlib.h>
-
-#include "helper.h"
-
 const int BUFF_SIZE = 1024 * 1024 * 8;
 
-void lidar_read_2368_data(LidarDataProcessor processor) {
-    debug("[lidar_read_2368_data]>>\n");
-    
-    char *p = malloc(BUFF_SIZE);
-    int offset = 0;
-    
-    if(processor) {
-        processor(p, offset, BUFF_SIZE);
-    }
-    
-    free(p);
-    debug("[lidar_read_2368_data]<<\n");
-}
-void lidar_read_8038_data(LidarDataProcessor processor) {
-    debug("[lidar_read_8038_data]>>\n");
-    char *p = malloc(BUFF_SIZE);
-    int offset = 0;
-    
-    if(processor) {
-        processor(p, offset, BUFF_SIZE);
-    }
-    
-    free(p);
-    debug("[lidar_read_8038_data]<<\n");
+int enableBroadCast(int fd) {
+	int broadcastEnable=1;
+	return setsockopt(fd, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable));
 }
 
-void lidar_write_10110_data(char *p, int start, int len) {
-    debug("[lidar_write_10110_data]>>\n");
-    debug("[lidar_write_10110_data]<<\n");
+LIDAR * lidar_send_init(int port) {
+	debug("[lidar_send_init]>>\n");
+	
+	LIDAR * p = (LIDAR *)malloc(sizeof(LIDAR));
+	
+	memset(&(p->addr),0,sizeof(struct sockaddr_in));
+    (p->addr).sin_family = AF_INET;
+    (p->addr).sin_addr.s_addr = inet_addr("255.255.255.255");
+    (p->addr).sin_port=htons(port);
+	
+	if ((p->fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		debug("Failed to open socket to UDP %d.", port);
+        return;
+    }
+    
+    if(enableBroadCast(fd) < 0) {
+		debug("Failed to enable broadcast.");
+    	return;
+    }
+	
+    debug("[lidar_send_init]<<\n");
+	
+	return p;
+}
+
+LIDAR * lidar_receive_init(int port) {
+	debug("[lidar_receive_init]>>\n");
+	
+	LIDAR * p = (LIDAR *)malloc(sizeof(LIDAR));
+	
+	memset(&(p->addr),0,sizeof(struct sockaddr_in));
+	
+	(p->addr).sin_family = AF_INET;
+	(p->addr).sin_addr.s_addr = htonl(INADDR_ANY);
+	(p->addr).sin_port = htons(port);
+	sin_len = sizeof(struct sockaddr_in);
+	
+	(p->fd) = socket(AF_INET, SOCK_DGRAM, 0);
+	bind((p->fd), (struct sockaddr *)&(p->addr), sizeof(struct sockaddr_in));
+	
+    debug("[lidar_receive_init]<<\n");
+	
+	return p;
+}
+
+void lidar_dispose(LIDAR * p) {
+	debug("[lidar_dispose]>>\n");
+	close(p->fd);
+	free(p);
+	debug("[lidar_dispose]<<\n");
+}
+
+void lidar_write_data(LIDAR * p, char * data, int start, int len) {
+    debug("[lidar_write_data]>>\n");
+	
+	if (sendto(p->fd, data, len, start,(struct sockaddr *) &(p->addr), sizeof(struct sockaddr_in)) < 0) {
+		debug("Failed to send UDP broadcast packet.");
+		return;
+	}
+	
+    debug("[lidar_write_data]<<\n");
+}
+
+void lidar_read_data(LIDAR * p, LidarDataProcessor processor) {
+    debug("[lidar_read_data]>>\n");
+	
+    char *buff = malloc(BUFF_SIZE);
+    int offset = 0;
+	int sin_len = sizeof(struct sockaddr);
+	
+	recvfrom((p->fd), buff, BUFF_SIZE, 0, (struct sockaddr *) &(p->addr), &sin_len);
+    
+    if(processor) {
+        processor(buff, offset, BUFF_SIZE);
+    }
+    
+    free(buff);
+    debug("[lidar_read_data]<<\n");
 }
 
 #ifdef __cplusplus
--- new/lidar.h
+++ new/lidar.h
@@ -5,12 +5,23 @@
 extern "C" {
 #endif
 
+const int LIDAR_DATA_PORT = 2368;
+const int LIDAR_SYNC_PORT = 8308;
+const int LIDAR_TIME_PORT = 10110;
+
 typedef void (* LidarDataProcessor)(char *p, int start, int len);
 
-void lidar_read_2368_data(LidarDataProcessor processor);
-void lidar_read_8038_data(LidarDataProcessor processor);
+typedef struct {
+	struct sockaddr_in addr;
+	int fd;
+} LIDAR;
 
-void lidar_write_10110_data(char *p, int start, int len);
+LIDAR * lidar_send_init(int port);
+LIDAR * lidar_receive_init(int port);
+void lidar_dispose(LIDAR * p);
+
+void lidar_read_data(LIDAR * p, LidarDataProcessor processor);
+void lidar_write_data(LIDAR * p, char * data, int start, int len);
 
 #ifdef __cplusplus
 }
