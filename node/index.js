660bcdee560be29b4c64e242ce9404cf9b73f478
QUAN <quan.shi.v5@outlook.com>
2016-07-29 02:28:40 +0000

    Add some addon code...

--- node/index.js
+++ node/index.js
@@ -1,13 +1,41 @@
-var    express = require('express');
-var    module1 = require('./build/Release/module1');
+var      express = require('express');
+var  compression = require('compression');
+var      module1 = require('./build/Release/module1');
 
 
 var app = express();
 
-console.log(module1.test());
+app.use(compression({filter: function (req, res) {
+  if (req.headers['x-no-compression']) {
+    return false;
+  }
+  
+  return compression.filter(req, res);
+}}));
+
+module1.call((msg) => {
+  console.log(msg);
+  console.log(module1.test(1, 2));
+  console.log(module1.newInstance('What\'s up?').msg);
+});
 
 app.get('/hello.action', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/events', function (req, res) {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+
+  var timer = setInterval(function () {
+    res.write('data: ping\n\n');
+
+    res.flush();
+  }, 2000);
+
+  res.on('close', function () {
+    clearInterval(timer);
+  });
 });
 
 app.use(express.static('../html'));
--- node/src/module1.cc
+++ node/src/module1.cc
@@ -6,12 +6,49 @@ namespace n {
 	void TestCall(const FunctionCallbackInfo<Value>& args) {
 		Isolate * isolate = args.GetIsolate();
 		
-		args.GetReturnValue().Set(String::NewFromUtf8(isolate, "This is test."));
+		if (args.Length() < 2) {
+			isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong number of arguments")));
+			return;
+		}
+		
+		if (!args[0]->IsNumber() || !args[1]->IsNumber()) {
+			isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong arguments")));
+			return;
+		}
+		
+		double value = args[0]->NumberValue() + args[1]->NumberValue();
+		Local<Number> num = Number::New(isolate, value);
+		
+		args.GetReturnValue().Set(num);
+		
+		//args.GetReturnValue().Set(String::NewFromUtf8(isolate, "This is test."));
 	}
 	
-	void init(Local<Object> exports) {
+	void RunCallback(const FunctionCallbackInfo<Value>& args) {
+		Isolate* isolate = args.GetIsolate();
+		Local<Function> cb = Local<Function>::Cast(args[0]);
+		const unsigned argc = 1;
+		Local<Value> argv[argc] = { String::NewFromUtf8(isolate, "hello world") };
+		cb->Call(Null(isolate), argc, argv);
+	}
+	
+	void CreateObject(const FunctionCallbackInfo<Value>& args) {
+		Isolate* isolate = args.GetIsolate();
+		
+		Local<Object> obj = Object::New(isolate);
+		obj->Set(String::NewFromUtf8(isolate, "msg"), args[0]->ToString());
+		
+		args.GetReturnValue().Set(obj);
+	}
+	
+	void init(Local<Object> exports, Local<Object> module) {
+		//NODE_SET_METHOD(module, "exports", RunCallback);
+		
+		NODE_SET_METHOD(exports, "call", RunCallback);
 		NODE_SET_METHOD(exports, "test", TestCall);
+		
+		NODE_SET_METHOD(exports, "newInstance", CreateObject);
 	}
 	
-	NODE_MODULE(test, init);
+	NODE_MODULE(module1, init);
 }
